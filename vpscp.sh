1ba33e9ed81e1a3d48cd59d8011fc19af363df58
ajbergh <53879585+ajbergh@users.noreply.github.com>
2020-02-24 11:29:55 -0600

    Update vpscp.sh

--- vpscp.sh
+++ vpscp.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-#########################################################################################
-#	        				Veeam Parallel scp Script									#
-#			        		(c)2019 adam.bergh@veeam.com								#
-#																						#
+#################################################################################################
+#	        		Veeam Parallel scp Script					#
+#			        (c)2019 adam.bergh@veeam.com					#
+#												#
 #  	Usage: vpscp.sh <source dir> <target server> <target dir> <number of threads> 		#
-#	Example: vpscp.sh /docs server2 /docs 8												#
-#																				    	#
-#	This script is designed to increase file copy throughput when syncing large			#
-#	file tree structures.																#
-#																						#
-#																						#
+#	Example: vpscp.sh /docs server2 /docs 8							#
+#												#
+#	This script is designed to increase file copy throughput when syncing large		#
+#	file tree structures.									#
+#												#
+#												#
 #	Performance Tuning: On destination host edit MaxSessions in /etc/ssh/sshd_config 	#
 #	(specifies the maximum number of open sessions permitted per network connection		#
-#	the default is set at 10) - Increase to thread count max. 							#
-#																						#
-#	Known issues: 																		#
-#	Any files with spaces or special characters will be skipped							#
-#	Job could fail if too many threads are used											#
-#																						#	
-#																						#
-#																						#
-#																						#
-#########################################################################################
+#	the default is set at 10) - Increase to thread count max. 				#
+#												#
+#	Known issues: 										#
+#	Any files with spaces or special characters will be skipped				#
+#	Job could fail if too many threads are used						#
+#												#	
+#												#
+#												#
+#												#
+#################################################################################################
 
 trap ctrl_c INT
 
